f66a92bb4b778e494e409794ba0c0f9c2828b80b
Brendon <brendonmccool@gmail.com>
2019-01-25 11:58:00 -0500

    Remove unused code

--- network.py
+++ network.py
@@ -49,7 +49,8 @@ class Network:
         outputErrors = targetVector - outputNetwork
 
         # Update the weights
-        #TODO how does this work? ... Here through the rest of self.train()
+        # TODO how does this work? 
+        # The rest of this is taken from https://www.python-course.eu/neural_network_mnist.php
         tmp = outputErrors * outputNetwork * (1.0 - outputNetwork)
         tmp = self.learningRate * np.dot(tmp.T, outputHidden)
         self.weights[1] += tmp.T
@@ -87,63 +88,3 @@ class Network:
         output_vector = np.dot(output_vector, self.weights[1])
         output_vector = sigmoid(output_vector)
         return output_vector
-
-
-
-class OldNetwork:
-    """Generic Nerual Network Class"""
-    # layer is an array containing all layers - input, hidden and output
-    # layer[0] is always the input layer, and is just an array of #TODO ints? floats?
-    # all other layers are based on a Neuron class
-    def __init__(self, numInputs, numOutputs, learningRate=0.1):
-        self.learningRate = learningRate
-        self.layer = []
-        self.layer.append([0] * numInputs)
-        #number of neurons in the prevous layer is
-        numWeights = len(self.layer[len(self.layer) - 1])
-        self.layer.append([Neuron(numWeights) for count in range(numOutputs)])
-
-    def run(self, input):
-        """ Run an image through the network """
-        # Transpose the input so we have a 1 by X matrix
-        # Note, ndmin=2 requires output to be 2d so that dimensions are 1 by X, and not just X
-        input_vector = np.array(input, ndmin=2).T
-
-        output_vector = np.dot()
-
-
-    def train(self, input, target):
-        logger.info("Training...")
-
-    def evaluate(self, images, labels):
-        logger.info("Evaluating...")
-        return 0, len(images)
-
-    def addLayer(self, numNeurons):
-        """
-        Add a new layer full of neurons.  All layers are added just prior to
-        the output stage.
-        """
-        #number of neurons in the next to last layer is
-        numWeights = len(self.layer[len(self.layer) - 2])
-        self.layer.insert(len(self.layer) - 1, [Neuron(numWeights) for count in range(numNeurons)])
-
-        #Now we need to update the weights for the last layer
-        #number of neurons in the next to last layer is
-        numWeights = len(self.layer[len(self.layer) - 2])
-        self.layer[len(self.layer) - 1] = [Neuron(numWeights) for count in range(len(self.layer[len(self.layer) - 1]))]
-
-    def status(self):
-        for index, layer in enumerate(self.layer):
-            logger.info("Layer {} has {} nodes".format(index, len(layer)))
-            if index > 0:
-                for node in layer:
-                    if len(node.weights) != len(self.layer[index - 1]):
-                        logger.info("Error! Node in layer {} has {} weights instead \
-                               of {}".format(index, len(node.weights), len(self.layer[index - 1])))
-
-class Neuron:
-    """Generic Neuron Class used by Neural Networks"""
-    # weights is an array containing all the weights from the previous layer
-    def __init__(self, numWeights):
-        self.weights = [0] * numWeights
